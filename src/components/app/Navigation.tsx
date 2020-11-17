73a8d8f06e827ba598d1f995f8ba60049078ce98
CBroaders <67294112+CBroaders12@users.noreply.github.com>
2020-11-17 09:42:27 -0500

    add navbar file

---
+++ src/components/app/Navigation.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class NavigationComponent extends Component<{/* props */}, {/* state */}> {
+  constructor(props: any /* TODO: Update this */) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default NavigationComponent;
