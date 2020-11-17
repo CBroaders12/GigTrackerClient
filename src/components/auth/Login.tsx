0a0948b8db864d965758b31b09e6f783ce6fe77d
CBroaders <67294112+CBroaders12@users.noreply.github.com>
2020-11-17 11:44:45 -0500

    start login component

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,20 +1,70 @@
-import React, { Component } from 'react';
+import React, { ChangeEvent, Component, FormEvent } from 'react';
 
-class LoginComponent extends Component<{/* props */}, {/* state */}> {
+type LoginState = {
+  email: string,
+  password: string,
+};
+
+class LoginComponent extends Component<{/* props */}, LoginState> {
   constructor(props: any /* TODO: Update this */) {
     super(props);
+    this.state = {
+      email: "",
+      password: "",
+    };
+
+    // Bind event handlers
+    this.updateEmail = this.updateEmail.bind(this);
+    this.updatePassword = this.updatePassword.bind(this);
+    this.onLoginSubmit = this.onLoginSubmit.bind(this);
+  }
+
+  async onLoginSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    if (this.state.email && this.state.password) {
+      let response = await fetch('http://localhost:5200/user/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: this.state.email,
+          password: this.state.password,
+        }),
+      });
+
+      let parsedResponse = await response.json();
+      console.log(parsedResponse);
+    } else {
+      alert('Enter email AND password');
+    }
+  }
+
+  updateEmail(event: ChangeEvent<HTMLInputElement>) {
+    this.setState({
+      email: event.target.value
+    })
+  }
+  
+  updatePassword(event: ChangeEvent<HTMLInputElement>) {
+    this.setState({
+      password: event.target.value
+    })
   }
 
   render() {
     return(
       <div>
-        <form>
+        <form onSubmit={this.onLoginSubmit}>
           <h2>Login</h2>
           <label htmlFor="loginEmail">Email</label>
-          <input type="email" id="loginEmail"/>
+          <input type="email" id="loginEmail" value={this.state.email} onChange={this.updateEmail}/>
 
           <label htmlFor="loginPassword">Password</label>
-          <input type="password" id="loginPassword"/>
+          <input type="password" id="loginPassword" value={this.state.password} onChange={this.updatePassword}/>
+
+          <button type="submit">Login</button>
         </form>
       </div>
     )
