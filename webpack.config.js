a91a359d9d829671007d50c4242146dc0e3fc2bd
CBroaders <67294112+CBroaders12@users.noreply.github.com>
2020-12-03 11:07:55 -0500

    webpack bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,71 @@
+// const path = require('path');
+
+// module.exports = {
+//   entry: {
+//     app: path.join(__dirname, 'src', 'index.tsx')
+//   },
+
+//   output: {
+//     filename: 'app.js',
+//     path: path.resolve(__dirname, 'dist')
+//   },
+
+//   module: {
+//     rules: [
+//       {
+//         test: /\.ts(x?)$/,
+//         exclude: /node_modules/,
+//         use: [
+//           {
+//             loader: "ts-loader"
+//           }
+//         ],
+//       },
+//       {
+//         test: /\.css$/,
+//         use: ["style-loader", "css-loader"]
+//       },
+      
+//       {
+//         enforce: "pre",
+//         test: /\.js$/,
+//         loader: "source-map-loader"
+//       }
+//     ]
+//   }
+// }
+
 const path = require('path');
 
 module.exports = {
-  entry: {
-    app: path.join(__dirname, 'dist', 'app.js')
-  },
-
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist')
-  },
-
+  entry: './src/index.tsx',
   module: {
     rules: [
       {
-        test: /\.ts(x?)$/,
+        test: /\.tsx?$/,
+        use: 'ts-loader',
         exclude: /node_modules/,
-        use: [
-          {
-            loader: "ts-loader"
-          }
-        ],
       },
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
       },
       {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      },
+      {
         enforce: "pre",
-        rest: /\.js$/,
+        test: /\.js$/,
         loader: "source-map-loader"
       }
-    ]
-  }
-}
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
