17973fcf75ee508caea33b4abf5280e9be8cf919
CBroaders <67294112+CBroaders12@users.noreply.github.com>
2020-12-03 11:50:26 -0500

    footer margin

--- src/components/app/Footer.tsx
+++ src/components/app/Footer.tsx
@@ -5,7 +5,7 @@ class Footer extends Component<{}, {}> {
 
   render() {
     return(
-      <Container id="footer" maxWidth={false}>
+      <Container id="footer" maxWidth={false} style={{marginTop: '-10%'}}>
         <p>&copy; Conor Broaders 2020</p>
       </Container>
     )
